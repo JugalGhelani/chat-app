637b3d0fb525a2aef4f5eda7b950212384c55a62
Jugal Ghelani <jugalghelani07@gmail.com>
2026-02-11 12:47:02 +0530

    feat: implement messaging module with socket.io integration

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,40 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import userRouter from "./routes/user.route.js";
+import messageRouter from "./routes/message.route.js";
+import { Server } from "socket.io";
+
 
 const app = express();
 const server = http.createServer(app);
+
+// Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+// store online users
+export const userSocketMap = {};  // {userId: socketId}
+
+// socket.io connection handler
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+    }
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User Disonnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        
+    })
+})
+
 
 // Middlewares
 app.use(cors());
@@ -15,5 +46,6 @@ app.use("/api/status", (req, res) =>
   res.send("Welcome to ChatApp Server is live"),
 );
 app.use("/api/auth", userRouter) // User routes
+app.use("/api/messages", messageRouter) // Message routes
 
 export { app, server };
---
+++ server/src/controllers/message.controller.js
@@ -0,0 +1,114 @@
+import { User } from "../models/user.model.js";
+import { Message } from "../models/message.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { io, userSocketMap } from "../app.js";
+
+// Get all users except the logged in user.
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const filteredUser = await User.find({ _id: { $ne: userId } }).select(
+      "-password",
+    );
+
+    //count number of messages not seen
+    const unseenMessages = {};
+    const promises = filteredUser.map(async (user) => {
+      const messages = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+    await Promise.all(promises);
+    res.json({ success: true, user: filteredUser, unseenMessages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get all messages for selected user
+export const getMessages = async (req, res) => {
+  try {
+    const { id: selectedUserId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: selectedUserId },
+        { senderId: selectedUserId, receiverId: myId },
+      ],
+    });
+
+    await Message.updateMany(
+      { senderId: selectedUserId, receiverId: myId },
+      { seen: true },
+    );
+    res.json({ success: true, messages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Api to mark message as seen using message id
+export const markMessageAsSeen = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Message.findByIdAndUpdate(id, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Send message to selected user
+export const sendMessage = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    if (!text && !req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "Message cannot be empty",
+      });
+    }
+
+    let imageUrl;
+    if (req.file) {
+      const cloudinaryResponse = await uploadOnCloudinary(req.file.path);
+
+      if (!cloudinaryResponse) {
+        return res.status(500).json({ message: "Image upload failed" });
+      }
+      imageUrl = cloudinaryResponse.secure_url;
+    }
+
+    const newMessage = await Message.create({
+        senderId,
+        receiverId,
+        text,
+        image: imageUrl 
+    })
+
+    // Emit the new message to the receiver's socket
+    const receiverSocketId = userSocketMap[receiverId];
+    console.log(receiverSocketId);
+    if (receiverSocketId) {
+        io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
+
+    res.json({success: true, newMessage}); 
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
